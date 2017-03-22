bc08ff30cbd803e8e6caaf862a635dad8992b71b
I-Ta Tsai@MBPR <ittsai@gmail.com>
2017-03-22 14:26:52 +0800

    can handle args.

--- db.py
+++ db.py
@@ -1,9 +1,12 @@
 import sqlite3
+import argparse
 
-conn = sqlite3.connect('trs-events.db')
-c = conn.cursor()
+def connect(sqlite_file):
+  conn = sqlite3.connect(sqlite_file)
+  c = conn.cursor()
+  return conn, c
 
-def create_table():
+def create_table(c):
   print 'Creating events table...'
   c.execute("CREATE TABLE events ( "
               "event_id integer PRIMARY KEY, "
@@ -12,16 +15,26 @@ def create_table():
               "start_date text NOT NULL, "
               "end_date text NOT NULL"
             ");")
-  conn.commit()
 
-def delete_events():
+def delete_events(c):
   print 'Deleting events in db...'
   c.execute("DELETE FROM events;")
-  conn.commit()
 
 def main():
-  #create_table()
-  delete_events()
+  sqlite_file = 'trs-events.db'
+  parser = argparse.ArgumentParser(description='Local db manipulation utility.')
+  group = parser.add_mutually_exclusive_group(required=True)
+  group.add_argument("-c", help="create event table", action="store_true")
+  group.add_argument("-d", help="delete events in db", action="store_true")
+  args = parser.parse_args()
+
+  conn, c = connect(sqlite_file)
+  if args.c:
+    create_table(c)
+  elif args.d:
+    delete_events(c)
+
+  conn.commit()
   conn.close()
 
 if __name__ == '__main__':
